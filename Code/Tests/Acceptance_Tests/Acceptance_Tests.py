93af95f6806b891b546c868ef7805ed98b6fd0d3
Nitzan Lary <laryn@post.bgu.ac.il>
2022-04-17 19:39:21 +0300

    added setup to acceptance tests

--- Code/Tests/Acceptance_Tests/Acceptance_Tests.py
+++ Code/Tests/Acceptance_Tests/Acceptance_Tests.py
@@ -5,22 +5,24 @@ service.initial_system()
 
 
 class AcceptanceTests(unittest.TestCase):
+    def setUp(self):
+        pass
 
-    def initial_system(self):
+    def test_initial_system(self):
         """
         I.1
 
         """
         pass
 
-    def contact_payment_service(self):
+    def test_contact_payment_service(self):
         """
         I.3
 
         """
         pass
 
-    def contact_supply_service(self):
+    def test_contact_supply_service(self):
         """
         I.4
 
@@ -32,27 +34,27 @@ class AcceptanceTests(unittest.TestCase):
     General guest actions
     """
 
-    def enter_as_guest(self):
+    def test_enter_as_guest(self):
         """
         II.1.1
 
         """
 
-    def exit(self):
+    def test_exit(self):
         """
         II.1.2
 
         """
         pass
 
-    def register(self):
+    def test_register(self):
         """
         II.1.3
 
         """
         pass
 
-    def login(self):
+    def test_login(self):
         """
         II.1.4
 
@@ -61,14 +63,14 @@ class AcceptanceTests(unittest.TestCase):
 
     """Guest's Purchase actions"""
 
-    def get_store_info(self):
+    def test_get_store_info(self):
         """
         II.2.1
 
         """
         pass
 
-    def search_product(self):
+    def test_search_product(self):
         """
         II.2.2
 
@@ -76,26 +78,26 @@ class AcceptanceTests(unittest.TestCase):
 
         pass
 
-    def add_product_to_shop_cart(self):
+    def test_add_product_to_shop_cart(self):
         """
         II.2.3
 
         """
         pass
 
-    def get_shop_cart(self):
+    def test_get_shop_cart(self):
         """
         II.2.4
 
         """
 
-    def remove_product_from_shop_cart(self):
+    def test_remove_product_from_shop_cart(self):
         """
         II.2.4
 
         """
 
-    def purchase_shop_cart(self):
+    def test_purchase_shop_cart(self):
         """
         II.2.5
 
@@ -104,84 +106,84 @@ class AcceptanceTests(unittest.TestCase):
 
     """Member's purchase actions"""
 
-    def logout(self):
+    def test_logout(self):
         """
         II.3.1
 
         """
         pass
 
-    def open_store(self):
+    def test_open_store(self):
         """
         II.3.2
 
         """
         pass
 
-    def review_product(self):
+    def test_review_product(self):
         """
         II.3.3
 
         """
         pass
 
-    def grade_product(self):
+    def test_grade_product(self):
         """
         II.3.4.1
 
         """
         pass
 
-    def grade_store(self):
+    def test_grade_store(self):
         """
         II.3.4.2
 
         """
         pass
 
-    def contact_store(self):
+    def test_contact_store(self):
         """
         II.3.5
 
         """
         pass
 
-    def complaint(self):
+    def test_complaint(self):
         """
         II.3.6
 
         """
         pass
 
-    def get_personal_purchase_history(self):
+    def test_get_personal_purchase_history(self):
         """
         II.3.7
 
         """
         pass
 
-    def get_personal_info(self):
+    def test_get_personal_info(self):
         """
         II.3.8
 
         """
         pass
 
-    def edit_personal_info(self):
+    def test_edit_personal_info(self):
         """
         II.3.8
 
         """
         pass
 
-    def upgrade_member_security(self):
+    def test_upgrade_member_security(self):
         """
         II.3.9
 
         """
         pass
 
-    # def add_products_to_inventory(self, user_id: str, store_id: str, products: dict):
+    # def test_add_products_to_inventory(self, user_id: str, store_id: str, products: dict):
     #     """
     #     II.4.1
     #
@@ -192,9 +194,9 @@ class AcceptanceTests(unittest.TestCase):
     #     """
     #     pass
     #
-    # def remove_products_to_inventory(self, user_id: str, store_id: str, products: dict):
+    # def test_remove_products_to_inventory(self, user_id: str, store_id: str, products: dict):
     #
-    def manage_inventory(self):
+    def test_manage_inventory(self):
         """
         II.4.1
         TODO: maybe to split this into 3 funcs???
@@ -203,7 +205,7 @@ class AcceptanceTests(unittest.TestCase):
         """
         pass
 
-    def edit_store_policy(self):
+    def test_edit_store_policy(self):
         """
         II.4.2
         *NOT*  need to implement to this version
@@ -211,133 +213,133 @@ class AcceptanceTests(unittest.TestCase):
         """
         pass
 
-    def change_purchase_policy(self):
+    def test_change_purchase_policy(self):
         """
         II.4.3.1
 
         """
         pass
 
-    def change_discount_policy(self):
+    def test_change_discount_policy(self):
         """
         II.4.3.2
 
         """
         pass
 
-    def add_store_owner(self):
+    def test_add_store_owner(self):
         """
         II.4.4
 
         """
         pass
 
-    def remove_store_owner(self):
+    def test_remove_store_owner(self):
         """
         II.4.5
 
         """
         pass
 
-    def add_store_manager(self):
+    def test_add_store_manager(self):
         """
         II.4.6
 
         """
         pass
 
-    def change_manager_permission(self):
+    def test_change_manager_permission(self):
         """
         II.4.7
 
         """
         pass
 
-    def remove_store_manager(self):
+    def test_remove_store_manager(self):
         """
         II.4.8
 
         """
         pass
 
-    def close_store(self):
+    def test_close_store(self):
         """
         II.4.9
 
         """
         pass
 
-    def reopen_store(self):
+    def test_reopen_store(self):
         """
         II.4.10
 
         """
         pass
 
-    def get_store_roles(self):
+    def test_get_store_roles(self):
         """
         II.4.11
 
         """
         pass
 
-    def get_users_messages(self):
+    def test_get_users_messages(self):
         """
         II.4.12.1
 
         """
         pass
 
-    def reply_users_messages(self):
+    def test_reply_users_messages(self):
         """
         II.4.12.2
 
         """
         pass
 
-    def get_store_purchase_history(self):
+    def test_get_store_purchase_history(self):
         """
         II.4.13
 
         """
         pass
 
-    def close_store_permanently(self):
+    def test_close_store_permanently(self):
         """
         II.6.1
 
         """
         pass
 
-    def remove_member(self):
+    def test_remove_member(self):
         """
         II.6.2
 
         """
         pass
 
-    def get_all_users_messages_by_admin(self):
+    def test_get_all_users_messages_by_admin(self):
         """
         II.6.3.1
 
         """
         pass
 
-    def reply_users_messages_by_admin(self):
+    def test_reply_users_messages_by_admin(self):
         """
         II.6.3.2
 
         """
         pass
 
-    def get_stores_purchase_history_by_admin(self):
+    def test_get_stores_purchase_history_by_admin(self):
         """
         II.6.4
 
         """
         pass
 
-    def get_system_statistic_by_admin(self):
+    def test_get_system_statistic_by_admin(self):
         """
         II.6.5
 
