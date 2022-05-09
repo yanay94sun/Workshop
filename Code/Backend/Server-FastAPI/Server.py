89ea971deec60532430a5bd2f11967a995873c72
Yanay <yanayshem@gmail.com>
2022-05-09 04:40:16 +0300

    no need for jwt, open store workes

--- Code/Backend/Server-FastAPI/Server.py
+++ Code/Backend/Server-FastAPI/Server.py
@@ -5,23 +5,25 @@ from pydantic.class_validators import Optional
 from random import randrange
 from fastapi.middleware.cors import CORSMiddleware
 # from Code.Backend.FastAPI import utils
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from fastapi.security import OAuth2PasswordBearer
 
 # print(os.getcwd())
 from passlib.context import CryptContext
 
-# hashing passwords
-
-
 from Code.Backend.Service.Objects.PaymentService import PaymentService
+from Code.Backend.Service.Objects.StoreName import Store_name
 from Code.Backend.Service.Objects.SupplySevice import SupplyService
 from Code.Backend.Service.Objects.TokenData import TokenData
 from Code.Backend.Service.Objects.User_info import User_info
 from Code.Backend.Service.Service import Service
+from Code.Backend import oauth2
 
 # from Code.Backend.FastAPI import utils
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="guests/login")
 
 service = Service()
 service.initial_system(payment_service=PaymentService(), supply_service=SupplyService())
@@ -82,8 +84,7 @@ def enter_as_guest(response: Response):
 
 
 @app.post("/guests/login")
-def login(user_info: User_info, user_id: Optional[str] = Cookie(None)):
-    print(user_id)
+def login(user_info: OAuth2PasswordRequestForm = Depends(), user_id: Optional[str] = Cookie(None)):
     # hashed_password = hash_pass(user_info.password)
     res = service.login(user_id, user_info.username, user_info.password)
     if res.error_occurred():
@@ -93,7 +94,7 @@ def login(user_info: User_info, user_id: Optional[str] = Cookie(None)):
     # create a token
     # return token
 
-    access_token = create_access_token(data={"user_id": user_id})
+    access_token = oauth2.create_access_token(data={"user_id": user_id})
 
     return {"access_token": access_token, "token_type": "bearer"}
 
@@ -133,12 +134,12 @@ def logout(user_id: Optional[str] = Cookie(None)):
     return Response(status_code=status.HTTP_200_OK)
 
 
-@app.post("users/open_store")
-def open_store(user_id: str, store_name: str):
-    res = service.open_store(user_id, store_name)
+@app.post("/users/open_store")
+def open_store(store_name: Store_name, user_id: Optional[str] = Cookie(None)):
+    res = service.open_store(user_id, store_name.store_name)
     if res.error_occurred():
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=res.msg)
-    return res
+    return Response(status_code=status.HTTP_200_OK)
 
 
 """
@@ -158,40 +159,44 @@ def hash_pass(password: str):
                 NEED TO MOVE TO DIFFERENT FILE, IMPORTS PROBLEMS!
 ------------------------------------------------------------------------
 """
-
-# SECRET_KEY
-# Algorithm
-# Expiration time
-
-SECRET_KEY = "09d25e049faa6ca2556a818196b7a3563b91f7099f6f0f4caa6cf63b88e2d9e6"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def create_access_token(data: dict):
-    __to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    __to_encode.update({"exp": expire})
-
-    encoded_jwt = jwt.encode(__to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-    return encoded_jwt
-
-
-def verify_access_token(token: str, user_exception):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-
-        user_id: str = payload.get("user_id")
-
-        if user_id is None:
-            raise user_exception
-        token_data = TokenData(id=user_id)
-
-    except JWTError:
-        raise user_exception
-
-    return token_data
-
-# def get_current_user(token: str = Depends())
+#
+# # SECRET_KEY
+# # Algorithm
+# # Expiration time
+#
+# SECRET_KEY = "09d25e049faa6ca2556a818196b7a3563b91f7099f6f0f4caa6cf63b88e2d9e6"
+# ALGORITHM = "HS256"
+# ACCESS_TOKEN_EXPIRE_MINUTES = 30
+#
+#
+# def create_access_token(data: dict):
+#     __to_encode = data.copy()
+#
+#     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     __to_encode.update({"exp": expire})
+#
+#     encoded_jwt = jwt.encode(__to_encode, SECRET_KEY, algorithm=ALGORITHM)
+#
+#     return encoded_jwt
+#
+#
+# def verify_access_token(token: str, user_exception):
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+#
+#         user_id: str = payload.get("user_id")
+#
+#         if user_id is None:
+#             raise user_exception
+#         token_data = TokenData(id=user_id)
+#
+#     except JWTError:
+#         raise user_exception
+#
+#     return token_data
+#
+#
+# def get_current_user(token: str = Depends(oauth2_scheme)):
+#     user_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+#                                    detail="could not validate user", headers={"WWW-Authenticate": "Bearer"})
+#     return verify_access_token(token, user_exception)
