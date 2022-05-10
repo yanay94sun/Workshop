12078d01738b6b6dbf2215268b2bdf75a68025b9
Yanay <yanayshem@gmail.com>
2022-05-10 17:27:25 +0300

    updated req

--- Code/Backend/Server-FastAPI/Server.py
+++ Code/Backend/Server-FastAPI/Server.py
@@ -45,7 +45,7 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
