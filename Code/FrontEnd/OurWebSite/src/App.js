b1542a2374f2ff72f2844e85911a0c1c94f29694
tomer <taltom@post.bgu.ac.il>
2022-05-11 16:38:17 +0300

    transition page

--- Code/FrontEnd/OurWebSite/src/App.js
+++ Code/FrontEnd/OurWebSite/src/App.js
@@ -4,36 +4,32 @@ import NoMatch from "./Pages/404Page/404Page";
 import Explore from "./Pages/Explore/Explore";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
-import NewHome from "./Pages/NewHome/NewHome";
+import Transition from "./Pages/Transition/Transition";
 import Register from "./Pages/Register/Register.js";
 import axios from "axios";
 
-axios.defaults.withCredentials = true
-
 class App extends React.Component {
   state ={
-    isLog:false,
-    isGuest: false,
+    isLog:false
   }
   handleLogin = (isLog) => this.setState({isLog})
   
   hendleEntrec = async () =>{
     // connecting to back
     // e.preventDefault();
-      const response = await axios.get('http://127.0.0.1:8000/guests/enter')
-      console.log(response.data)
-      this.setState({[this.isGuest]:true})
+    const response = await axios.get('http://127.0.0.1:8000/guests/enter')
+    console.log(response)
   }
 
   render(){
     const {isLog} = this.state;
-       this.hendleEntrec();
-
+    //this.hendleEntrec();
 
   return (
     <div>
       <Routes >
-        <Route path = '/' element= {<Login isLogin={this.handleLogin} />}/>
+        <Route path = '/' element= {<Transition/>}/>
+        <Route path = '/Login' element= {<Login isLogin={this.handleLogin} />}/>
         <Route path = '/home/*' element = {<Home handleLogged={this.handleLogin}/>}/>
         <Route path ="/register" element= {<Register/>}/>
         <Route path = '*' element={<NoMatch/>}/>
