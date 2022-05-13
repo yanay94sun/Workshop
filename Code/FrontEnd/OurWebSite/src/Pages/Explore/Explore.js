c1e86a69bebf8861c13312da569ab97502348664
tomer <taltom@post.bgu.ac.il>
2022-05-13 14:08:00 +0300

    store update

--- Code/FrontEnd/OurWebSite/src/Pages/Explore/Explore.js
+++ Code/FrontEnd/OurWebSite/src/Pages/Explore/Explore.js
@@ -1,31 +1,19 @@
-import React from "react";
+import React , {useState} from "react";
 import "./Explore.css"
 import axios from 'axios'
 
 function Explore(){
-    //("<li>" + {input} + "</li>")
-    var state ={
-        text:'',
-    }
-
-    const handleChange = (e) =>{
-        const {value} = e.target
-        state = value
-    }
+    const [text, setText] = useState("")
 
     const handleSumbit = async (e) =>{
         e.preventDefault()
-        const storeName = {
-            text: state.text,
-        }
-        //const response = await axios.post("http://127.0.0.1:8000/users/open_store",storeName)
-        //console.log(response)
+        
     }
 
     return(
         <div>
             <div className="div-list">
-                <input type="text" placeholder="Search for anything" onChange = {handleChange}/> 
+                <input type="text" placeholder="Search for anything" onChange = {(e) => setText(e.target.value)}/> 
                 <button style={{cursor:'pointer'}} onClick={handleSumbit}>search</button>
                 <div className="products-type">
                     <a href="/products/food">
--- Code/FrontEnd/OurWebSite/src/Pages/Store/Store.js
+++ Code/FrontEnd/OurWebSite/src/Pages/Store/Store.js
@@ -1,19 +1,41 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 import axios from 'axios'
 
 
 function Store({products}){
+    const [storeName, setStoreName] = useState('');
+    const [storeRank, setRank] = useState('');
+    const [founderName, setFounderName] = useState('')
+    const [storesProducts, setStoresProducts] = useState([])
     const params = useParams()
     const storeId = params.storeId
     const getStoreInfo = async () =>{
+        try{
         const response = await axios.get("http://127.0.0.1:8000/stores/"+storeId,{storeId:storeId})
         console.log(response)
+        setStoreName(response.data["store_name"])
+        setFounderName(response.data["founder_id"])
+        setRank(response.data["rank"])
+        setStoresProducts(storesProducts => [...storesProducts,...response.data["products"]])
+        } catch (err){
+            console.log(err.response);
+        }
     }
+    useEffect(() => {   
+        getStoreInfo()
+      }, []);
 
 return(
     <div>
-        <h2>Store id is {storeId}</h2>
+        <h2>Store details:</h2>
+        <ul>
+            <li>store name: {storeName}</li>
+            <li>store founder's name: {founderName}</li>
+            <li>store ranking: {storeRank}</li>
+            <li>store products: {storesProducts.length == 0 ? "no products": storesProducts}</li>
+        </ul>
+        
     </div>
 )
 }
