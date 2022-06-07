7313f1de5ef8199bbc871beecb7013cc57099003
refael-farjun <refaelfarjun5@gmail.com>
2022-06-07 16:24:16 +0300

    change class to function in ShoppingCart.js - not finish

--- Code/FrontEnd/OurWebSite/src/Pages/ShoppingCart/ShoppingCart.js
+++ Code/FrontEnd/OurWebSite/src/Pages/ShoppingCart/ShoppingCart.js
@@ -1,76 +1,84 @@
-import React, { Component, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/navbar';
-import Counters from './components/counters.jsx';
+import Counters from './components/counters.js';
 
-class ShoppingCart extends Component {
-	state = {
-		counters: [
-			{ id: 1, value: 5, tags: ['tag1', 'tag2', 'tag3'] },
-			{ id: 2, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
-			{ id: 3, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
-			{ id: 4, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
-		],
+function ShoppingCart() {
+	// state = {
+	// 	counters: [
+	// 		// { id: 1, value: 5, tags: ['tag1', 'tag2', 'tag3'] },
+	// 		// { id: 2, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
+	// 		// { id: 3, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
+	// 		// { id: 4, value: 0, tags: ['tag1', 'tag2', 'tag3'] },
+	// 	],
+	// };
+	const [counters, setCounters] = useState([{ id: 1, value: 5, tags: ['tag1', 'tag2', 'tag3'] },
+											{ id: 2, value: 0, tags: ['tag1', 'tag2', 'tag3'] },]);
+
+
+	const handleDelete = (counterId) => {
+		const counters_filtered = counters.filter((c) => c.id !== counterId);
+		setCounters({ counters_filtered });
 	};
 
-	handleDelete = (counterId) => {
-		const counters = this.state.counters.filter((c) => c.id !== counterId);
-		this.setState({ counters });
-	};
-
-	handleIncrement = (counter) => {
-		const counters = [...this.state.counters];
-		const idx = counters.indexOf(counter);
-		counters[idx] = { ...counter };
-		counters[idx].value++;
-		this.setState({ counters });
+	const handleIncrement = (counter) => {
+		const counters2inc = [...counters];
+		const idx = counters2inc.indexOf(counter);
+		counters2inc[idx] = { ...counter };
+		counters2inc[idx].value++;
+		setCounters({ counters2inc });
 	};
 	//
-	handleDicrement = (counter) => {
-		const counters = [...this.state.counters];
-		const idx = counters.indexOf(counter);
-		counters[idx] = { ...counter };
-		if (counters[idx].value > 0) counters[idx].value--;
-		this.setState({ counters });
+	const handleDicrement = (counter) => {
+		const counters2dic = [...this.state.counters];
+		const idx = counters2dic.indexOf(counter);
+		counters2dic[idx] = { ...counter };
+		if (counters2dic[idx].value > 0) counters2dic[idx].value--;
+		setCounters({ counters2dic });
 	};
 
-	handleReset = () => {
-		const counters = this.state.counters.map((c) => {
+	const handleReset = () => {
+		const counters2rest = counters.map((c) => {
 			c.value = 0;
 			return c;
 		});
-		this.setState({ counters });
+		setCounters({ counters2rest });
 	};
-	render() {
+
+	useEffect(() => {   
+        // getStoreInfo()
+      }, []);
+
+	// render() {
 		// console.log("App rendered !!")
-		return (
-			<React.Fragment>
-				<NavBar
-					totalCounters={
-						this.state.counters.filter((c) => c.value !== 0).length
-					}
+	return (
+		<React.Fragment>
+			<NavBar
+				totalCounters={
+					counters.filter((c) => c.value !== 0).length
+				}
+			/>
+			<main
+				className='container'
+				style={{
+					position: 'fixed',
+					left: 0,
+					// width: 300,
+				}}>
+				<button style={{ cursor: 'pointer' }}>Buy Now !</button>
+				<h4 style={{ color: 'red' }}>
+					Discount available only for today! hurry up!!
+				</h4>
+				<Counters
+					counters={counters}
+					onDelete={handleDelete}
+					onIncrement={handleIncrement}
+					onDicrement={handleDicrement}
+					onReset={handleReset}
 				/>
-				<main
-					className='container'
-					style={{
-						position: 'fixed',
-						left: 0,
-						// width: 300,
-					}}>
-					<button style={{ cursor: 'pointer' }}>Buy Now !</button>
-					<h4 style={{ color: 'red' }}>
-						Discount available only for today! hurry up!!
-					</h4>
-					<Counters
-						counters={this.state.counters}
-						onDelete={this.handleDelete}
-						onIncrement={this.handleIncrement}
-						onDicrement={this.handleDicrement}
-						onReset={this.handleReset}
-					/>
-				</main>
-			</React.Fragment>
-		);
-	}
+			</main>
+		</React.Fragment>
+	);
+// }
 }
 
 export default ShoppingCart;
