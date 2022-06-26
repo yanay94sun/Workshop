f088b51de6d5481aa6acd8959fc42dde6123b26a
Yanay <yanayshem@gmail.com>
2022-06-26 21:13:52 +0300

    started writing support for external services api, need to check with front, just start

--- Code/Backend/Domain/PaymentServiceAdapter.py
+++ Code/Backend/Domain/PaymentServiceAdapter.py
@@ -12,13 +12,14 @@ class PaymentServiceAdapter:
 
     def pay(self, domain_payment_info):
         if self.__payment_service is not None:
-            PARAMS = {"action_type", "pay"}, \
-                     {"card_number", domain_payment_info.card_number},\
-                     {"month", domain_payment_info.month}, \
-                     {"year", domain_payment_info.year}, \
-                     {"holder", domain_payment_info.holder},\
-                     {"ccv", domain_payment_info.ccv}, \
-                     {"id", domain_payment_info.id}
+            PARAMS = {"action_type", "pay",
+                      "card_number", domain_payment_info.card_number,
+                      "month", domain_payment_info.month,
+                      "year", domain_payment_info.year,
+                      "holder", domain_payment_info.holder,
+                      "ccv", domain_payment_info.ccv,
+                      "id", domain_payment_info.id
+                      }
             # res = self.__payment_service.pay(domain_payment_info.credit_card,
             #                                         domain_payment_info.cvv,
             #                                         domain_payment_info.amount_to_pay)
