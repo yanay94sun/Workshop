84976213d18b9a4c6ed699582051260aaa1ab950
Asaf Stern <asafste@post.bgu.ac.il>
2022-06-27 17:26:52 +0300

    maybe async fix

--- Code/Backend/Domain/Publisher/NotificationController.py
+++ Code/Backend/Domain/Publisher/NotificationController.py
@@ -8,6 +8,8 @@ from Code.Backend.Domain.VisitorStates.MemberState import MemberState
 
 import Code.Backend.Server_FastAPI.messages_sender as server
 
+import asyncio
+
 
 class Activities(Enum):
     PURCHASE_IN_STORE = 0
@@ -22,6 +24,9 @@ class NotificationController:
         self.__uc = user_controller
         self.__stores_activity: Dict[str, List[List[str]]] = {}  # store_id, list<list<str usernames>>
         self.__phone_book: Dict[str, WebSocket] = {}  # uid, websocket
+
+        self.__event_loop = asyncio.BaseEventLoop()
+        self.__event_loop.run_forever()
 
     def subscribe(self, username: str, store_id: str, activity: Activities):
         self.__stores_activity[store_id][activity.value].append(username)
@@ -41,19 +46,25 @@ class NotificationController:
             print(f"sending to {username}")
             self.notify_single(username, msg)
 
-    def notify_single(self, to_username, content):
+
+    async def notify_single_task(self, to_username, content):
         accepted_msg = False
         print("in notify_single")
         if self.__uc.is_online(to_username):
             print("notify single 1")
             uid = self.__uc.get_username_uid(to_username)
             print("notify single 2")
-            accepted_msg = server.send_ws_message(content, self.__phone_book[uid])
+            accepted_msg = await server.send_ws_message(content, self.__phone_book[uid])
         if not accepted_msg:
             print("notify single 3")
             res = self.__uc.add_message_to_member(to_username, content)
             if res.error_occurred():
                 return res
+
+    def notify_single(self, to_username, content):
+        task = self.__event_loop.create_task(self.notify_single_task(to_username, content))
+        self.__event_loop.run_until_complete(task)
+
 
     def register_connection(self, uid, ws: WebSocket):
         print("notify single 4")
